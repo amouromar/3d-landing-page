0a43675c946a2c1450143185db6d971a1d69e71a
Amour Omar <amourhamisiomar@gmail.com>
2025-05-20 00:33:18 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="page-lg">
+      <div className="wrapper">
+        <header>
+          <nav>
+            <h1 className="text-dark-100 font-satoshi">3D Landing Page</h1>
+          </nav>
+        </header>
+      </div>
+    </div>
+  )
+}
+
+export default page
